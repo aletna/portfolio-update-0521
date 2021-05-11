a5c60aba1853be79fb7f255a0b7cce56d63607cc
Dennis Antela Martinez <dennis.antela@gmail.com>
2021-05-11 15:51:32 -0700

    center navbar items

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,23 +7,33 @@ const Navbar = () => {
     <header className="sticky top-0 z-10 bg-white shadow p-3  text-sm text-gray-700">
       <div className="flex w-full container mx-auto justify-between">
         {/* Left */}
-        <div className="flex space-x-4 items-center">
-          <p className="linkNav ">Code</p>
+        <div className="flex w-1/4 space-x-4 items-center">
+          <p className="linkNav" onClick={() => router.push("/portfolio")}>
+            Dev Portfolio
+          </p>
           {/* <p className="linkNav">About</p> */}
           {/* <p className="linkNav">Store</p> */}
         </div>
         {/* Center  */}
         <div
-          className="flex  items-center cursor-pointer"
+          className="flex w-2/4 items-center cursor-pointer"
           onClick={() => router.push("/")}
         >
-          <Avatar url="https://firebasestorage.googleapis.com/v0/b/dennis-portfolio-942f1.appspot.com/o/dennis.JPG?alt=media&token=bb846abf-f773-459e-ad0a-4e9b531406ef" />
-          {/* <Sidenav /> */}
+          <div className="mx-auto">
+            <Avatar url="https://firebasestorage.googleapis.com/v0/b/dennis-portfolio-942f1.appspot.com/o/dennis.JPG?alt=media&token=bb846abf-f773-459e-ad0a-4e9b531406ef" />
+            {/* <Sidenav /> */}
+          </div>
         </div>
         {/* Right */}
-        <div className="flex space-x-4 items-center">
-          <p className="linkNav">Music</p>
-          {/* <p className="linkNav ">Code</p> */}
+        <div className="flex w-1/4 space-x-4  items-center">
+          <div className="ml-auto">
+            <p className="linkNav">
+              <a href="https://aletna.me" target="_blank">
+                Music Portfolio
+              </a>
+            </p>
+            {/* <p className="linkNav ">Code</p> */}
+          </div>
 
           {/* Icon */}
           {/* <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" /> */}
